89df99a762edffdbc3ccc125e45f44cf9ca52672
youngtae0519 <youngtae0519k@gmail.com>
2025-09-17 03:05:49 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long int n;
+
+int main() {
+  cin.tie(0) -> ios::sync_with_stdio(0);
+
+  cin >> n;
+
+  cout << (n * (n - 1) * (n - 2)) / 6 << "\n3\n";
+
+  return 0;
+}
