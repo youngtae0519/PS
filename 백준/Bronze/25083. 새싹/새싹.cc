1c72b3cc60c4b8239009f00639765b9d339be429
youngtae0519 <youngtae0519k@gmail.com>
2025-09-17 02:46:36 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cin.tie(0) -> ios::sync_with_stdio(0);
+
+  cout << "         ,r'\"7\n";
+  cout << "r`-_   ,'  ,/\n";
+  cout << " \\. \". L_r'\n";
+  cout << "   `~\\/\n";
+  cout << "      |\n";
+  cout << "      |\n";
+
+  return 0;
+}
