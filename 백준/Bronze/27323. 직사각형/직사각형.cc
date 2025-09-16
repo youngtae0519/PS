e1765f5b4ca3db64c58324ce5c9a974acd20d05a
youngtae0519 <youngtae0519k@gmail.com>
2025-09-17 03:00:11 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int A, B;
+
+int main() {
+  cin.tie(0) -> ios::sync_with_stdio(0);
+
+  cin >> A >> B;
+
+  cout << A * B << "\n";
+
+  return 0;
+}
