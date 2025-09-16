3d058067b1ed7faa44e56b4f149c098a988b7f26
youngtae0519 <youngtae0519k@gmail.com>
2025-09-17 02:32:23 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int T, A, B, total;
+
+  cin >> T;
+  total = T;
+
+  while(T--) {
+    cin >> A >> B;
+
+    cout << "Case #" << total - T << ": " << A << " + " << B << " = " << A + B << "\n";
+  }
+
+  return 0;
+}
