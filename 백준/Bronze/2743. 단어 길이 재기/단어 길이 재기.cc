99c4294210e3713b543d7e8e7b685d714b8803fd
youngtae0519 <youngtae0519k@gmail.com>
2025-11-24 00:30:02 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+string s;
+
+int main() {
+  cin.tie(0) -> ios::sync_with_stdio(0);
+
+  cin >> s;
+
+  cout << s.length() << '\n';
+  
+  return 0;
+}
