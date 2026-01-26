d1fc327a09bf6cfc17fa1a49221d0236d58e3b1d
Taeyoung Kim <youngtae0519k@gmail.com>
2026-01-27 00:18:55 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+
+int main() {
+    cin.tie(0) -> ios::sync_with_stdio(0);
+    
+    cin >> N;
+    
+    cout << N * (N - 1);
+    
+    return 0;
+}
