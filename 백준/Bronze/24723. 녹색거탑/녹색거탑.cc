59284ab106fb8cdb151a56bb0977c41b3eac1506
Taeyoung Kim <youngtae0519k@gmail.com>
2026-01-27 00:52:54 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+
+int main() {
+    cin.tie(0) -> ios::sync_with_stdio(0);
+    
+    cin >> N;
+    
+    cout << pow(2, N);
+    
+    return 0;
+}
