077bfbbd5b61c319bc466e44ddde7f38d1dea197
Taeyoung Kim <youngtae0519k@gmail.com>
2026-01-27 15:24:58 +0900

    [Bronze I] Title: 이항 계수 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, K;
+int* arr;
+
+int main() {
+    cin.tie(0) -> ios::sync_with_stdio(0);
+    
+    cin >> N >> K;
+    
+    arr = new int[N + 1];
+    
+    arr[0] = 1;
+    arr[1] = 1;
+    for(int i = 2; i <= N; i++) {
+        arr[i] = arr[i - 1] * i;
+    }
+    
+    cout << arr[N] / (arr[K] * arr[N - K]);
+    
+    return 0;
+}
