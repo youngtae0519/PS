e64b4453598db801c3b7fc93710609b8353dcdac
Taeyoung Kim <youngtae0519k@gmail.com>
2026-01-30 13:52:06 +0900

    [Silver V] Title: 올림픽, Time: 104 ms, Memory: 110576 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -5,20 +5,22 @@ for _ in range(N):
 
 arr.sort(key = lambda x : (-x[1], -x[2], -x[3]))
 
-g, s, b = 0, 0, 0
-ans = 1
-dup = 0
+g, s, b = -1, -1, -1
+ans = 0
+dup = 1
 for item in arr:
     if item[0] == K:
-        if item[1] != g or item[2] != s or item[3] != b:
-            ans += 1
+        if g == -1:
+            ans = 1
+        elif item[1] != g or item[2] != s or item[3] != b:
+            ans += dup
         break
     cur = item[1 : ]
     if cur[0] == g and cur[1] == s and cur[2] == b:
         dup += 1
     else:
         ans += dup
-        dup = 0
-        g, c, b = cur
+        dup = 1
+        g, s, b = cur
         
 print(ans)
