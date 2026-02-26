062ff5b870505772ff3763b976e5f9ca270fe97d
Taeyoung Kim <youngtae0519k@gmail.com>
2026-02-26 14:23:45 +0900

    [Silver II] Title: 한 줄로 서기, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
@@ -5,17 +5,8 @@ input = sys.stdin.readline
 N = int(input())
 arr = list(map(int, input().rstrip().split()))
 
-cnt = 0
 ans = []
-while True:
-    if cnt == N:
-        break
-    for i in range(N):
-        if arr[i] == 0:
-            ans.append(str(i + 1) + ' ')
-            for j in range(i + 1):
-                arr[j] -= 1
-            break
-    cnt += 1
+for i in range(N - 1, -1, -1):
+    ans.insert(arr[i], i + 1)
     
-print(''.join(ans))
+print(*ans)
