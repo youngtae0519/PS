7cd276e59916ee8908c53d613b67a1787a3092aa
Taeyoung Kim <youngtae0519k@gmail.com>
2026-03-03 12:18:46 +0900

    [Silver I] Title: 회전 초밥, Time: 144 ms, Memory: 113296 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.py
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -1,4 +1,5 @@
 import sys
+from collections import Counter
 
 input = sys.stdin.readline
 
@@ -7,14 +8,31 @@ N, d, k, c = map(int, input().split())
 sushi = []
 for _ in range(N):
     sushi.append(int(input()))
+    
+dic = Counter(sushi[ : k])
 
-ans = 0
+tmp = len(dic.keys())
+
+if dic[c] == 0:
+    ans = tmp + 1
+    have = False
+else:
+    ans = tmp
+    have = True
+
 for i in range(N):
-    tmp = set()
-    for j in range(k):
-        tmp.add(sushi[(i + j) % N])
-    if c not in tmp:
-        tmp.add(c)
-    ans = max(ans, len(tmp))
-
+    dic[sushi[i]] -= 1
+    if dic[sushi[i]] == 0:
+        tmp -= 1
+        del dic[sushi[i]]
+        
+    dic[sushi[(i + k) % N]] += 1
+    if dic[sushi[(i + k) % N]] == 1:
+        tmp += 1
+    
+    if dic[c] == 0:
+        ans = max(ans, tmp + 1)
+    else:
+        ans = max(ans, tmp)
+        
 print(ans)
