bfcadf43418e1dd4dbda21ee4f40d7fbf8650267
Taeyoung Kim <youngtae0519k@gmail.com>
2026-03-11 12:45:48 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 160 ms, Memory: 118908 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -5,17 +5,21 @@ input = sys.stdin.readline
 
 N, K = map(int, input().split())
 
-time = [sys.maxsize for _ in range(200001)]
+time = [float('inf') for _ in range(200001)]
 time[N] = 0
 
 dq = deque([[N, 0]])
 
 while len(dq) > 0:
     cur = dq.popleft()
+    
+    if cur[0] == K:
+        print(cur[1])
+        break
         
     if cur[0] * 2 <= 200000 and time[cur[0] * 2] > cur[1]:
         time[cur[0] * 2] = cur[1]
-        dq.append([cur[0] * 2, cur[1]])
+        dq.appendleft([cur[0] * 2, cur[1]])
         
     if cur[0] + 1 <= 200000 and time[cur[0] + 1] > cur[1] + 1:
         time[cur[0] + 1] = cur[1] + 1
@@ -24,5 +28,3 @@ while len(dq) > 0:
     if cur[0] - 1 >= 0 and time[cur[0] - 1] > cur[1] + 1:
         time[cur[0] - 1] = cur[1] + 1
         dq.append([cur[0] - 1, cur[1] + 1])
-        
-print(time[K])
