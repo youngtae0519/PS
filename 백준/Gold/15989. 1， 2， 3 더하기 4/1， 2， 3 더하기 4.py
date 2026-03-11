6369743978182d129ea36d074ae3fe9c5d957e5e
Taeyoung Kim <youngtae0519k@gmail.com>
2026-03-11 12:55:36 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 124 ms, Memory: 120780 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
@@ -2,22 +2,20 @@ import sys
 
 input = sys.stdin.readline
 
+MAX_SIZE = 100001
+dp = [[0 for _ in range(4)] for _ in range(MAX_SIZE)]
+dp[0][1] = 1
+dp[0][2] = 1
+dp[0][3] = 1
+
+for i in range(1, MAX_SIZE):
+    dp[i][1] = 1
+    dp[i][2] = dp[i][1] + (dp[i - 2][2] if i >= 2 else 0)
+    dp[i][3] = dp[i][2] + (dp[i - 3][3] if i >= 3 else 0)
+    
 T = int(input())
-ans = []
 
 for _ in range(T):
     n = int(input())
     
-    cnt = 1 + n // 2
-
-    while True:
-        n -= 3
-        
-        if n < 0:
-            break
-            
-        cnt += 1 + n // 2
-    
-    ans.append(str(cnt) + '\n')
-    
-print(''.join(ans))
+    print(dp[n][3])
