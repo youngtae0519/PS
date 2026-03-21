e4223990d21409f2c9ab485ab76173172a1cde2c
Taeyoung Kim <youngtae0519k@gmail.com>
2026-03-21 10:30:33 +0900

    [Gold V] Title: 탑, Time: 376 ms, Memory: 224444 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -2,22 +2,22 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-height = list(map(int, input().rstrip().split()))
+arr = list(map(int, input().rstrip().split()))
 
-stack = [[1, height[0]]]
+stack = [[1, arr[0]]]
 ans = [0]
 
 for i in range(1, N):
     while stack:
-        if stack[-1][1] > height[i]:
+        if stack[-1][1] > arr[i]:
             ans.append(stack[-1][0])
-            stack.append([i+1, height[i]])
+            stack.append([i+1, arr[i]])
             break
         else:
             stack.pop()
-    
+            
     if not stack:
-        stack.append([i+1, height[i]])
+        stack.append([i+1, arr[i]])
         ans.append(0)
         
 print(*ans)
