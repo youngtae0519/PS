c14d51be26810adbd670925b1d4853a3d1a41777
Taeyoung Kim <youngtae0519k@gmail.com>
2026-03-23 16:06:39 +0900

    [Gold V] Title: 용액, Time: 116 ms, Memory: 125492 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -4,31 +4,24 @@ input = sys.stdin.readline
 N = int(input())
 arr = list(map(int, input().split()))
 
-left, right = 0, 0
 hap = float('inf')
-for i in range(N):
-    start = i + 1
-    end = N - 1
+left = 0
+right = N - 1
+ansLeft, ansRight = 0, 0
+
+while left < right:
+    cur = arr[left] + arr[right]
     
-    while start <= end:
-        mid = (start + end) // 2
-        
-        if arr[i] + arr[mid] == 0:
-            left = i
-            right = mid
-            break
-        elif arr[i] + arr[mid] > 0:
-            if abs(arr[i] + arr[mid]) < hap:
-                hap = abs(arr[i] + arr[mid])
-                left = i
-                right = mid
-            end = mid - 1
-        else:
-            if abs(arr[i] + arr[mid]) < hap:
-                hap = abs(arr[i] + arr[mid])
-                left = i
-                right = mid
-            start = mid + 1
+    if abs(cur) < hap:
+        hap = abs(cur)
+        ansLeft = left
+        ansRight = right
+    
+    if cur == 0:
+        break
+    elif cur > 0:
+        right -= 1
+    else:
+        left += 1
 
-
-print(arr[left], arr[right])
+print(arr[ansLeft], arr[ansRight])
