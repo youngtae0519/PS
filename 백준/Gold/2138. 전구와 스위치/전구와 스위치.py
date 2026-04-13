7985b4310c95750c35308ba3851550e0b9215f24
Taeyoung Kim <youngtae0519k@gmail.com>
2026-04-13 20:02:56 +0900

    [Gold IV] Title: 전구와 스위치, Time: 112 ms, Memory: 120268 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
@@ -1,37 +1,34 @@
 import sys
 input = sys.stdin.readline
 
-def change(b):
-    return 1 if b == 0 else 0
+N = int(input())
+start = list(map(int, input().rstrip()))
+end = list(map(int, input().rstrip()))
 
-def switch(bulb, idx):
-    for i in [idx - 1, idx, idx + 1]:
-        if 0 <= i < N:
-            bulb[i] = change(bulb[i])
-
-def simulate(now, goal, first):
-    bulb = now[:]
+def simulate(start, first):
+    bulb = start[:]
     cnt = 0
     
     if first:
-        switch(bulb, 0)
+        bulb[0] = 1 - bulb[0]
+        bulb[1] = 1 - bulb[1]
         cnt += 1
-    
+        
     for i in range(1, N):
-        if bulb[i - 1] != goal[i - 1]:
-            switch(bulb, i)
+        if bulb[i - 1] != end[i - 1]:
+            bulb[i - 1] = 1 - bulb[i - 1]
+            bulb[i] = 1 - bulb[i]
+            if i != N - 1:
+                bulb[i + 1] = 1 - bulb[i + 1]
             cnt += 1
             
-    return cnt if bulb == goal else float('inf')
+    if bulb == end:
+        return cnt
+    else:
+        return float('inf')
 
-N = int(input())
-now = list(map(int, input().rstrip()))
-goal = list(map(int, input().rstrip()))
-
-
-res1 = simulate(now, goal, False)
-res2 = simulate(now, goal, True)
-
-ans = min(res1, res2)
+first_on = simulate(start, True)
+first_off = simulate(start, False)
+ans = min(first_on, first_off)
 
 print(ans if ans != float('inf') else -1)
