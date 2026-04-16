72ffc6d787a26706fe6d6317e6ca41bb8e96f816
Taeyoung Kim <youngtae0519k@gmail.com>
2026-04-16 12:47:09 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 116 ms, Memory: 122160 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.py
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.py
@@ -4,26 +4,17 @@ input = sys.stdin.readline
 N = int(input())
 nums = list(map(int, input().rstrip().split()))
 
-ans = N
-cnt = [0 for _ in range(100001)]
-cnt[nums[0]] = 1
-start, end = 0, 0
+ans = 0
+cnt = [0] * 100001
+start = 0
 
-while start < N:
-    if end < N - 1:
-        end += 1
-        cnt[nums[end]] += 1
-        
-        if cnt[nums[end]] == 2:
-            while nums[start] != nums[end]:
-                ans += (end - start - 1)
-                cnt[nums[start]] -= 1
-                start += 1
-            ans += (end - start - 1)
-            cnt[nums[start]] -= 1
-            start += 1
-    elif end == N - 1:
-        ans += (end - start)
+for end in range(N):
+    cnt[nums[end]] += 1
+    
+    while cnt[nums[end]] > 1:
+        cnt[nums[start]] -= 1
         start += 1
+        
+    ans += (end - start + 1)
     
 print(ans)
