56c2a613897a467e931c64f0288f0b58cbd8a4ac
Taeyoung Kim <youngtae0519k@gmail.com>
2026-04-22 18:09:41 +0900

    [Gold IV] Title: 여행 가자, Time: 104 ms, Memory: 110576 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -0,0 +1,48 @@
+import sys
+input = sys.stdin.readline
+
+def find(x):
+    if parent[x] < 0:
+        return x
+    
+    parent[x] = find(parent[x])
+    return parent[x]
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+    
+    if x == y:
+        return
+    
+    if parent[x] < parent[y]:
+        parent[y] = x
+    elif parent[y] < parent[x]:
+        parent[x] = y
+    else:
+        parent[x] = y
+        parent[y] -= 1
+        
+    return
+
+N = int(input())
+M = int(input())
+parent = [-1] * (N + 1)
+
+for i in range(N):
+    arr = list(map(int, input().rstrip().split()))
+    
+    for j in range(N):
+        if arr[j]:
+            union(i + 1, j + 1)
+            
+plan = list(map(int, input().rstrip().split()))
+root = find(plan[0])
+ans = "YES"
+
+for i in range(1, M):
+    if root != find(plan[i]):
+        ans = "NO"
+        break
+        
+print(ans)
