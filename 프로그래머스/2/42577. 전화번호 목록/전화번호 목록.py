388337fb06785cebc7bee4ff80bb28d1b68ba380
Taeyoung Kim <youngtae0519k@gmail.com>
2026-04-25 12:21:19 +0900

    [level 2] Title: 전화번호 목록, Time: 107.62 ms, Memory: 29.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,24 +1,10 @@
-from collections import defaultdict
-
 def solution(phone_book):
-    answer = True    
-        
-    cnt = defaultdict(int)
-    s = set()
-    for num in phone_book:
-        cnt[int(num)] = 1
-        s.add(len(num))
-
-    s = list(s)
-
-    for num in phone_book:
-        for length in s:
-            if len(num) <= length:
-                continue
-            if cnt[int(num) // (10 ** (len(num) - length))] == 1:
-                answer = False
-                break
-        if not answer:
+    answer = True
+    phone_book.sort()
+    
+    for i in range(len(phone_book) - 1):
+        if phone_book[i + 1].startswith(phone_book[i]):
+            answer = False
             break
-                
+            
     return answer
