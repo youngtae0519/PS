ab3c487932e556acb9dce7fbf6abd8439e66486f
Taeyoung Kim <youngtae0519k@gmail.com>
2026-05-22 23:46:10 +0900

    [level 2] Title: 타겟 넘버, Time: 166.07 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,15 @@
+answer = 0
+
+def dfs(idx, numbers, num, target):
+    global answer
+    if idx == len(numbers):
+        if num == target:
+            answer += 1
+    else:
+        dfs(idx + 1, numbers, num + numbers[idx], target)
+        dfs(idx + 1, numbers, num - numbers[idx], target)
+
+def solution(numbers, target):
+    dfs(1, numbers, numbers[0], target)
+    dfs(1, numbers, -numbers[0], target)
+    return answer
